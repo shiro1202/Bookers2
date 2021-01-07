ca339d2af2c8f023c8ebb40076e2aeee8b43c9f3
shiro1202 <black0157@icloud.com>
2021-01-07 12:13:07 +0000

    fixed

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd2fd35cde0ece08726b88466afc97b03ebe4b5f2a438047ae381e635966576ad0614a1c7098ad7327ef97318f58adb9b0dc70622eeaf86c7c9eb325e51bb3ad2'
