dc92b640730863ede25f337e598dc693380437e6
shiro1202 <black0157@icloud.com>
2021-01-17 05:24:56 +0000

    complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   before_action :check_same_book, only: [:edit, :update, :destroy]
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -14,23 +14,24 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
     @user = current_user
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -40,24 +41,24 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def check_same_book
     @book = Book.find(params[:id])
     unless @book.user_id == current_user.id
       redirect_to books_path
     end
   end
-  
+
 end
